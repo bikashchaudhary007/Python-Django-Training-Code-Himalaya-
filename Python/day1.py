01c44c91d90bb6123538d04987a16000fe0c757d
Bikash <bikashchaudhary007@gmail.com>
2022-07-28 08:27:23 +0545

    First Commit

---
+++ Python/day1.py
@@ -0,0 +1,37 @@
+# print("Helloworld")
+# a = 7
+# print(type(a))
+# print(dir(__builtins__))
+# print(dir(int))
+# print(help(list))
+# print(help(str))
+# import math
+# print(math.__doc__)
+# q,r = divmod(13, 5)
+# print(q)
+# print(r)
+
+# print(13%5)
+
+# list1 = [1,2,"name"]
+# list1.append("Bikash")
+# print(list1)
+
+#slicing [start: stop: step]
+#negative indexing [::-1]
+
+# list1 = []
+# for x in range (6):
+#     list1.append(x)
+# print(list1[::-1])
+
+# res = [x for x in range(6)] #List comprehension
+# print(res[::-1])
+
+
+
+
+
+
+
+
---
+++ Python/day2.py
@@ -0,0 +1,37 @@
+# list=["abc","def",3, -6, 5]
+# list1 = [each for each in list if type(each)==int and each>0]
+# print(list1)
+
+# def transpose_matrix():
+#     matrix1 = [[1,2],[3,4],[5,6],[7,8]]
+#     transposed = [[row[i] for row in matrix1] for i in range(len(matrix1[0]))]
+#     print(transposed)
+
+# transpose_matrix()
+
+# def matrix():
+#     matrix1 = [[1,2],[3,4],[5,6],[7,8]]
+#     for row in matrix1:
+#         print(row)
+#         for i in range(2):
+#             print(row[i])
+
+# matrix() 
+
+
+# a = {'1':'pending', '2': 'approved', '3': 'abcd'}
+# if a.values() == 'abc':
+#     print(a.keys())
+
+
+# #set
+# s = {1, 'abc', 2, 3}
+# print(type(s))
+
+
+
+
+
+
+
+
